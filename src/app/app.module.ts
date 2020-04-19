0a1d97ba9d55d5d75e6ea68aa1a7db2977b9f514
rajanimmana1 <debugtest369@gmail.com>
2020-04-20 00:14:49 +0530

    2 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
@@ -9,7 +10,7 @@ import { ConvertToSpacePipe } from './shared/convert-to-space.pipe'
 import { StarComponent } from './shared/star.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule , HttpClientModule ],
   declarations: [ AppComponent, HelloComponent, ProductListComponent , ConvertToSpacePipe, StarComponent ],
   bootstrap:    [ AppComponent ]
 })
--- src/app/product/product.service.ts
+++ src/app/product/product.service.ts
@@ -1,34 +1,18 @@
 import { Injectable } from "@angular/core";
 import { Iproduct } from "./product";
+import { HttpClient } from '@angular/common/http';
+import { Observable } from "rxjs";
+import { catchError,tap } from 'rxjs/operators';
+
 
 @Injectable({
   providedIn :'root'
 })
 export class ProductService{
-
-  getProducts():Iproduct[]{
-    return[
-       {
-      "productId" : 1,
-      "productName": "car",
-      "productCost":52.35,
-      "productYear":2020,
-      "productCode" : "#1c-b",
-       "productTitle" : "first",
-       "ProductRating" : 4.3,
-       "imageUrl" : "https://images.app.goo.gl/Wv4EXUnFnURFCD9P8" 
-    },
-    {
-      "productId" : 2,
-      "productName": "bike",
-      "productCost":35.76,
-      "productYear":2021,
-      "productCode" : "#2c-b",
-      "productTitle" : "second",
-      "ProductRating" : 4.7,
-       "imageUrl" : "https://images.app.goo.gl/U2Mu8nkcG61czrCH7"
-    }
-    ]
+  private productUrl = 'app/products/products.json'
+  constructor(private http:HttpClient){}
+  getProducts():Observable<Iproduct[]>{
+    return this.http.get<Iproduct[]>(this.productUrl);
   }
 
 }
